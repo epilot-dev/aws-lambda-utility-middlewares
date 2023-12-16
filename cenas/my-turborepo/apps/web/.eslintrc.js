3cc4b3d6813fc13967d40f7f79bf09960e3ee2be
Turbobot <turbobot@vercel.com>
2023-12-16 01:53:37 +0000

    feat(create-turbo): apply official-starter transform

--- cenas/my-turborepo/apps/web/.eslintrc.js
+++ cenas/my-turborepo/apps/web/.eslintrc.js
@@ -1,7 +1,7 @@
-// This configuration only applies to the package manager root.
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  ignorePatterns: ["apps/**", "packages/**"],
+  root: true,
+  extends: ["@repo/eslint-config/next.js"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: true,
