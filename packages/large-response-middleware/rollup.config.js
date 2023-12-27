ea755b82b1a22ea20df22aef766464f95dd523f2
João Pinho <j.pinho@epilot.cloud>
2023-12-27 10:56:11 +0000

    chore: fix module exports

--- packages/large-response-middleware/rollup.config.js
+++ packages/large-response-middleware/rollup.config.js
@@ -1,28 +1,29 @@
 // rollup.config.js
-import typescript from "@rollup/plugin-typescript";
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import babel from "@rollup/plugin-babel";
-import json from "@rollup/plugin-json";
-import pkg from "./package.json";
+import babel from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
 import sourcemaps from 'rollup-plugin-sourcemaps';
-import { visualizer } from "rollup-plugin-visualizer";
+import { visualizer } from 'rollup-plugin-visualizer';
 
-const extensions = [".js", ".jsx", ".ts", ".tsx"];
+import pkg from './package.json';
 
-const isProdBuild = process.env.NODE_ENV === "production";
+const extensions = ['.js', '.jsx', '.ts', '.tsx'];
+
+const isProdBuild = process.env.NODE_ENV === 'production';
 
 export default {
-  input: "src/index.ts", 
+  input: 'src/index.ts',
   output: [
     {
-      file: "lib/cjs/index.js",
-      format: "cjs", // CommonJS format for Node.js
+      file: 'lib/cjs/index.js',
+      format: 'cjs', // CommonJS format for Node.js
       sourcemap: true,
     },
     {
-      file: "lib/index.js",
-      format: "esm", // ES module format for modern browsers
+      file: 'lib/index.js',
+      format: 'esm', // ES module format for modern browsers
       sourcemap: true,
     },
   ],
@@ -35,14 +36,15 @@ export default {
     resolve({ preferBuiltins: true, extensions }), // Resolve module paths
     commonjs(), // Convert CommonJS modules to ES6
     sourcemaps(),
-    babel({ extensions, include: ["src/**/*"], babelHelpers: "bundled" }), // Transpile to ES5 and include polyfills
-    !isProdBuild && visualizer({
-      emitFile: false,
-      filename: "stats.html",
-      gzipSize: true,
-      brotliSize: true,
-      open: true,
-    }),
+    babel({ extensions, include: ['src/**/*'], babelHelpers: 'bundled' }), // Transpile to ES5 and include polyfills
+    !isProdBuild &&
+      visualizer({
+        emitFile: false,
+        filename: 'stats.html',
+        gzipSize: true,
+        brotliSize: true,
+        open: false,
+      }),
   ],
   external: Object.keys(pkg.dependencies || {}), // Don't bundle dependencies
 };
